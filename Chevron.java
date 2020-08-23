b1da3725ab54dec09ad836691167f4596293dff6
adizafri2000 <61119610+adizafri2000@users.noreply.github.com>
2020-08-24 03:57:42 +0800

    Random Update

--- Chevron.java
+++ Chevron.java
@@ -1,13 +1,16 @@
 /**
  * Chevron class defines structure of a Chevron Piece object.
  * @author Adi
- * @version 1.01, 21/8/20
+ * @version %I%
  */
 public class Chevron extends Piece{
+
+    int[][] movements;
 
     public Chevron(String name,char side,int x,int y){
         super(name, side,x,y);
     }
+
 
     @Override
     public void move() {
