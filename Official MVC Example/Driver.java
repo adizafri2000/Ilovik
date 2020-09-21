d6b4281febceb1c9b13590f886d414faa9ec4737
adizafri2000 <61119610+adizafri2000@users.noreply.github.com>
2020-09-21 23:53:14 +0800

    cleanup

--- Official MVC Example/Driver.java
+++
@@ -1,9 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        Marker model = new Marker("red");
-        View view = new View();
-        Controller controller = new Controller(model,view);
-        controller.initListeners();
-
-    }
-}
