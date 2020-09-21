e8c4180ffb05185dc4044b37139892629199da76
adizafri2000 <61119610+adizafri2000@users.noreply.github.com>
2020-09-21 23:51:34 +0800

    JAVADOCS

--- Controller.java
+++ Controller.java
@@ -409,16 +409,4 @@ public class Controller implements ActionListener {
         emptyViewBoardIcons();
         setViewBoardIcons();
     }
-    //test controller
-    public static void main(String[] args){
-        View v = new View();
-        Game g = new Game();
-        Player p1 = new Player("Kamal", 'b');
-        p1.setTurn(true);
-        Player p2 = new Player("Adli", 'r');
-        Board b = new Board (p1, p2,false);
-        g = new Game(b);
-        Controller c = new Controller(v,g,p1,p2,b);
-        c.initController();
-    } 
 }
